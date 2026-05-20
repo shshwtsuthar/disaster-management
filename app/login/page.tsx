8819ff06810ecba2402753dc7fdc0bb437656870
shshwtsuthar <shshwtsuthar@gmail.com>
2026-05-20 13:12:18 +1000

    fix: register page, login page top bar

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,8 @@
-import TopBar from "@/components/TopBar";
 import ReliefWorkerLogin from "@/components/ReliefWorkerLogin";
 
 export default function LoginPage() {
   return (
     <div className="flex flex-col flex-1 bg-zinc-50 font-sans dark:bg-black">
-      <TopBar />
       <ReliefWorkerLogin />
     </div>
   );
---
+++ app/register/page.tsx
@@ -0,0 +1,9 @@
+import ReliefWorkerRegistration from "@/components/ReliefWorkerRegistration";
+
+export default function RegisterPage() {
+  return (
+    <div className="flex flex-col flex-1 bg-zinc-50 font-sans dark:bg-black">
+      <ReliefWorkerRegistration />
+    </div>
+  );
+}
