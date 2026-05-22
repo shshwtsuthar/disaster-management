ee50f446d01e2d2499fa12458f786b64b6a9aa40
shshwtsuthar <shshwtsuthar@gmail.com>
2026-05-22 12:45:39 +1000

    feat: added a stub for dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function DashboardPage() {
+  return (
+    <div className="flex flex-col flex-1 bg-zinc-50 font-sans dark:bg-black">
+      <h1>Dashboard</h1>
+      <p>Welcome to the dashboard</p>
+      <p>This is the dashboard page</p>
+      <p>This is the dashboard page</p>
+    </div>
+  );
+}
